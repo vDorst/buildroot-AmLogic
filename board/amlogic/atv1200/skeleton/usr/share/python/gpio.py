76e7e528917a13e2607fc7c478dc610587886124
Re van Dorst <opensource@vdorst.com>
2013-04-22 21:39:16 +0200

    GPIO: Updated Pythonscript. Show changed value in red.

--- board/amlogic/atv1200/skeleton/usr/share/python/gpio.py
+++ board/amlogic/atv1200/skeleton/usr/share/python/gpio.py
@@ -5,14 +5,14 @@ MASK = [            ## O = AO, T = BOOT
 	0xFC000000, ## PREG_PAD_2  .... ..DD DDDD DDDD CCCC CCCC CCCC CCCC 
 	0xFF0C0000, ## PREG_PAD_3  .... .... XXXX ..TT TTTT TTTT TTTT TTTT 
 	0x00000000, ## PREG_PAD_4  XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX
-	0xFF800000, ## PREG_PAD_5  .... .... .YYY YYYY YYYY YYYY YYYY YYYY 
+	0xFF800003, ## PREG_PAD_5  .... .... .YYY YYYY YYYY YYYY YYYY YYYY 
 	0xF800F000, ## PREG_PAD_6  .... .ZZZ ZZZZ ZZZZ .... EEEE EEEE EEEE
-	0xFFFFF000  ## PREG_PAD_AO .... .... .... .... .... OOOO OOOO OOOO 
+	0xFFFFF003  ## PREG_PAD_AO .... .... .... .... .... OOOO OOOO OOOO 
 ]
 
 def CharToHex(Char):
-	s = '0123456789ABCDEF'
-	for i in range(0, 16):
+	s = '0123456789ABCDEF '
+	for i in range(0, 17):
 		if s[i] == Char: 
 			return { 
 				0: "0000",
@@ -24,13 +24,14 @@ def CharToHex(Char):
 				6: "0110",
 				7: "0111",
 				8: "1000",
-				8: "1001",
+				9: "1001",
 				10: "1010",
 				11: "1011",
 				12: "1100",
 				13: "1111",
 				14: "1110",
 				15: "1111",
+				16: "0000"
 			}.get(i)
 	return "----"
 		
@@ -42,6 +43,17 @@ def StrToHex(Str):
 	for i in range(len(s)):
 		retstr = retstr + CharToHex(s[i])
 	return retstr
+old_data = [
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000",
+	"00000000000000000000000000000000"
+]
+ 
 
 while(1):
 	gpio = open('/sys/class/gpio/cmd', 'r')
@@ -51,18 +63,21 @@ while(1):
 	gpio.readline()
 	for line in gpio:
 		data[bank] = line.rstrip('\n')
-		##print data[bank]
+		# print data[bank]
 		bank = bank + 1
 	gpio.close()
 
-	cOutput = '1'
+	cOutput = '0'
 	print "BANK: 1098-7654-3210-9876-5432-1098-7654-3210"
 	bank_data = {}
 	for bank in range(len(data)):
-		bank_data["NAME"] = data[bank][:4].strip()
-		bank_data["EN"]   = StrToHex(data[bank][7:15].strip())
-		bank_data["OUT"]  = StrToHex(data[bank][17:25].strip())
-		bank_data["IN"]   = StrToHex(data[bank][27:35].strip())
+		bank_data["NAME"] = data[bank][:5].strip()
+		bank_data["EN"]   = StrToHex(data[bank][7:15])
+		bank_data["OUT"]  = StrToHex(data[bank][17:25])
+		bank_data["IN"]   = StrToHex(data[bank][27:35])
+		# print  bank_data["EN"]
+		# print  bank_data["OUT"]
+		# print  bank_data["IN"] 
 		bank_data["STATUS"]  = ""
 		txt = ""
 		bit = 0x80000000
@@ -72,13 +87,20 @@ while(1):
 			if (MASK[bank] & bit):
 				txt = "."
 			else:
+
 				if (bank_data["EN"][i] == cOutput):
 					txt = "_"
 				else:
-					txt = bank_data["IN"][i]
+					if (bank_data["IN"][i] == old_data[bank][i]):
+						txt = bank_data["IN"][i]
+					else:
+						txt = "\x1b[1;31m"+ bank_data["IN"][i] + "\x1b[0m"
 			bank_data["STATUS"] = bank_data["STATUS"] + txt
 			bit = bit >> 1
 
 		print bank_data["NAME"] + "  : " + bank_data["STATUS"]
-	time.sleep(1)
+		old_data[bank] = bank_data["IN"]
+	#time.sleep(1)
+	raw_input("a")
+        print "read:"	
 
